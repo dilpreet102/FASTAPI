cbc174be9878e1c7a91725a7ad4b9720dbb7c482
Fastapi <ds5327618@gmail.com>
2024-04-02 17:19:59 +0530

    implemented CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app import models
 from app.database import engine
@@ -9,6 +11,15 @@ from app.config import settings
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
